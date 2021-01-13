5deac46db625aa53974794d315abbdf6a9f94293
AnnaP93 <popovichann56@gmail.com>
2021-01-13 10:33:17 -0500

    Add description to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wine_reviews
+This is the background analysis of the wine reviews dataset that was found on Kaggle - https://www.kaggle.com/zynicide/wine-reviews
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import pandas as pd
 reviews = pd.read_csv("input/winemag-data-130k-v2.csv", index_col=0)
 reviews.info()
 print(reviews.isnull().sum())
+print("All the columns except 'description', 'points', 'title' and 'winery' include missing values.")
 print(reviews.describe(include=[np.number]))
       
