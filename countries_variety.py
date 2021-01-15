9598cc3c8ed8b3bbc59e5d39158838fe655813d9
AnnaP93 <popovichann56@gmail.com>
2021-01-15 12:00:02 -0500

    Added function to get the most common wine-producers

--- countries_variety.py
+++ countries_variety.py
@@ -28,16 +28,45 @@ def get_avg_price_per_type():
         x = avg_price[wine_type]
         print('{0} - {1}'.format(wine_type, round(x)))
 
-
 # get_avg_price_per_type()
+
 
 def get_wineries_by_country():
     winery_volume_by_country = reviews()[["country", "winery"]].groupby(by="country").count().sort_values(by="winery", ascending=False)
     return winery_volume_by_country
 
-
 # get_wineries_by_country()
 
+
+def find_common_wine_producers():
+    filling_na_in_region_1 = reviews().region_1.fillna("Unknown")
+    count_regions = filling_na_in_region_1.groupby(filling_na_in_region_1.values).count().sort_values(ascending=False)
+    return count_regions
+
+# print(find_common_wine_producers())
+
+
+def find_common_wine_producers_and_countries():
+    # how it should look:
+    # Region (Country): Wine Count
+    # Unknown (): 21247
+    # Napa Valley (US): 4480
+    # Columbia Valley (WA) (Canada): 4124
+    # .....
+    reviews_copy = reviews()
+    reviews_copy.region_1.fillna("Unknown", inplace=True)
+    # print(reviews_copy.region_1.isnull().sum())
+    region_country = reviews_copy.groupby('region_1').apply(lambda x: x.country.iloc[0])
+    if region_country.loc['Unknown']:
+        region_country.loc['Unknown'] = ''
+
+    for element in find_common_wine_producers().items():
+        region = element[0]
+        count = element[1]
+        print('{0} ({1}): {2}'.format(region, region_country[region], count))
+
+
+# find_common_wine_producers_and_countries()
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from read_input import wine_reviews as reviews
-from countries_variety import get_top_5_popular_wines, get_avg_price_per_type, get_wineries_by_country
-
+from countries_variety import get_top_5_popular_wines, get_avg_price_per_type, get_wineries_by_country, find_common_wine_producers_and_countries
+from reviews_and_prices import get_price_extremes, get_min_price_per_point, get_max_points_per_price, get_high_ranking_low_price
 
 def initial_analysis():
     reviews().info()
@@ -14,17 +14,20 @@ def initial_analysis():
 
 
 def execute():
-    print('Initial dataset analysis:')
-    initial_analysis()
+    # print('Initial dataset analysis:')
+    # initial_analysis()
+    #
+    # print('Top 5 most popular types of wines are:')
+    # print(get_top_5_popular_wines())
+    #
+    # print("The average prices for each type of the most popular wines are presented below:")
+    # get_avg_price_per_type()
+    #
+    # print("The number of wineries broken down by countries:")
+    # print(get_wineries_by_country())
 
-    print('Top 5 most popular types of wines are:')
-    print(get_top_5_popular_wines())
-
-    print("The average prices for each type of the most popular wines are presented below:")
-    get_avg_price_per_type()
-
-    print("The number of wineries broken down by countries:")
-    print(get_wineries_by_country())
+    print('The most common wine-producing regions sorted by the number of wines produced in each of the regions:')
+    print(find_common_wine_producers_and_countries())
 
 
 execute()
